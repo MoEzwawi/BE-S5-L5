4089416f44b1dc83c57be2e8fee7090d56ff2b59
MoEzwawi <mezwawi96@gmail.com>
2024-01-12 12:28:02 +0100

    initializes spring boot project

---
+++ src/main/java/MoEzwawi/BES5L5/BeS5L5Application.java
@@ -0,0 +1,13 @@
+package MoEzwawi.BES5L5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeS5L5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeS5L5Application.class, args);
+	}
+
+}
